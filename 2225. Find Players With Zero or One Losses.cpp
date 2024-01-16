2ab684d34b05ebf428a6c2b8bee1be0b9a401b7a
suksham <129354626+suksham11@users.noreply.github.com>
2024-01-16 10:27:31 +0530

    Create 2225. Find Players With Zero or One Losses.cpp

---
+++ 2225. Find Players With Zero or One Losses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+         unordered_map<int , int> lost_map;
+        
+        for(int i=0 ; i<matches.size(); i++ ){
+            int loser = matches[i][1];
+
+            lost_map[loser]++;
+        }
+
+        vector<int> notlost;
+        vector<int> lostonce;
+
+        for(int i=0 ; i<matches.size() ; i++){
+            int winner = matches[i][0];
+            int loser = matches[i][1];
+
+            if(lost_map.find(winner) == lost_map.end()){
+            notlost.push_back(winner);
+            lost_map[winner] = 2;
+        }
+        
+
+        if(lost_map[loser] == 1){
+            lostonce.push_back(loser);
+        }
+        }
+        sort(begin(lostonce) , end(lostonce));
+        sort(begin(notlost), end(notlost));
+
+        return{notlost , lostonce};
+    }
+};
